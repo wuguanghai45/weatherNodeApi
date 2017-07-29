2be96b4465aa2f5933c305ea2a0a571028cbf8c6
walt <walt@agideo.com>
2017-07-29 14:18:58 +0800

    允许跨域

--- app/app.js
+++ app/app.js
@@ -7,6 +7,9 @@ app.get('/', function (req, res) {
     res.send("");
   } else {
     weather(req.query['cityname'], (string)=> {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+      res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
       res.setHeader('Content-Type', 'text/plain');
       res.send(string);
     })
